b60b4c880ca4dad160a546f50f0cc127087fd915
KingMob <john@onethreethree.net>
2021-09-17 16:28:32 +0100

    fix testing change

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const {
   LocalStorageCryptoStore,
 } = require("matrix-js-sdk/lib/crypto/store/localStorage-crypto-store");
 
-const userInfo = JSON.parse(localStorage.getItem("userInf"));
+const userInfo = JSON.parse(localStorage.getItem("userInfo"));
 let matrixClient;
 
 const trustAllDevices = () => {
